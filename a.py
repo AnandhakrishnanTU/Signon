639ce02a6867aa9c950b907707d29a34cd48da37
AnandhakrishnanTU <anandhakrishnantu.snm19cs006@gmail.com>
2023-10-16 11:35:14 +0530

    files updated

--- a.py
+++
@@ -1,5 +0,0 @@
-list=['V','A','R','N','A','M']
-list.append('E')
-print(list)
-list.sort
-print(list)s
--- try2.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-text_speech=pyttsx3.init()
-
-anser=input("say something :")
-
-text_speech.say(anser)
-
-text_speech.runAndWait()
